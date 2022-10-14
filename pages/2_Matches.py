2578fb5836ccbdd91bab8a38ae752a7f10e2c52c
Wazarr <jeje_04@live.com>
2022-10-14 19:59:59 +0200

    fix pagination matches

--- pages/2_Matches.py
+++ pages/2_Matches.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 from prisma import Prisma
 from prisma.models import LeagueMatch
-from st_aggrid import AgGrid, AgGridTheme
+from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 from utils.utils import hide_streamlit_elements
@@ -72,20 +72,35 @@ def main():
     teams_list = get_teams(db)
     divisions_list = get_divisions(db)
 
-    matchday_options = set([m.matchday for m in matches_list])
-    pagination_division = (
-        len(matches_list) / len(divisions_list) / max(matchday_options)
-    )
-    pagination_team = max(matchday_options) / 2
+    matchday_options = {
+        div.id: set([m.matchday for m in matches_list if m.leagueDivisionId == div.id])
+        for div in divisions_list
+    }
+
+    pagination_division = {
+        div.id: len(
+            [
+                m
+                for m in matches_list
+                if m.matchday == 1 and div.id == m.leagueDivisionId
+            ]
+        )
+        for div in divisions_list
+    }
+
+    pagination_team = {
+        div.id: len(matchday_options[div.id]) / 2 for div in divisions_list
+    }
 
     st.write("# Season 3 matches")
 
     col1, col2, col3 = st.columns([3, 2, 9])
     with col1:
-        div_select = st.selectbox(
+        div_name_select = st.selectbox(
             "Division",
             [d.name for d in divisions_list],
         )
+        div_select = [d for d in divisions_list if d.name == div_name_select][0]
     with col2:
         st.text("")
         st.text("")
@@ -95,7 +110,9 @@ def main():
         )
     with col3:
         if use_team_filter:
-            team_options = [t.name for t in teams_list if t.division.name in div_select]
+            team_options = [
+                t.name for t in teams_list if t.division.name in div_name_select
+            ]
         else:
             team_options = []
         team_options.sort()
@@ -104,18 +121,23 @@ def main():
             team_options,
         )
 
+    matchdays_options_div = matchday_options[div_select.id]
+
     matchdays_select = st.slider(
         "Matchdays",
-        min_value=min(matchday_options),
-        max_value=max(matchday_options),
-        value=(min(matchday_options), max(matchday_options)),
+        min_value=min(matchdays_options_div),
+        max_value=max(matchdays_options_div),
+        value=(
+            min(matchdays_options_div),
+            max(matchdays_options_div),
+        ),
     )
 
     match_list_filter = []
     for m in matches_list:
         if m.matchday < matchdays_select[0] or m.matchday > matchdays_select[1]:
             continue
-        if m.LeagueDivision.name != div_select:
+        if m.LeagueDivision.name != div_name_select:
             continue
         if team_select is None or any(
             [md.team.name == team_select for md in m.LeagueMatchDetail]
@@ -133,9 +155,9 @@ def main():
     )
 
     if use_team_filter:
-        pagination_nb = pagination_team
+        pagination_nb = pagination_team[div_select.id]
     else:
-        pagination_nb = pagination_division
+        pagination_nb = pagination_division[div_select.id]
 
     gb.configure_pagination(
         enabled=True,
