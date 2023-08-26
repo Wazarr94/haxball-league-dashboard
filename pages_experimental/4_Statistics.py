b4f89c864a5ce93b4fa396894006671e97295e10
Wazarr <jeje_04@live.com>
2023-08-26 09:52:23 +0200

    fix stats fr

--- pages_experimental/4_Statistics.py
+++ pages_experimental/4_Statistics.py
@@ -408,8 +408,8 @@ def main():
     matchdays_values = range(len(matchdays_options_div))
     matchday_max = get_max_matchday_stats(matches_list, div_select)
     if len(matchdays_options_div) == 0:
-        matchdays_options_div = [1, 1]
-        matchday_max = 1
+        matchdays_values = [0]
+        matchday_max = 0
 
     matchdays_select = st.select_slider(
         "Matchdays",
